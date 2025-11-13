72ec4cb4009c448cd14dba4faff82fd74862361d
Yusuf Muratov[muratov-yusuf] <work.yusuf.muratov@gmail.com>
2025-11-13 23:44:19 +0300

    refactor: add navigation icon to AppToolbar and remove boilerplate files

--- core/ui/src/main/java/dev/ymuratov/core/ui/components/AppToolbar.kt
+++ core/ui/src/main/java/dev/ymuratov/core/ui/components/AppToolbar.kt
@@ -4,11 +4,15 @@ import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AppToolbar(title: String) {
-    CenterAlignedTopAppBar(title = { Text(title) })
+fun AppToolbar(title: String, modifier: Modifier = Modifier, navigationIcon: (@Composable () -> Unit)? = null) {
+    CenterAlignedTopAppBar(
+        modifier = modifier,
+        title = { Text(title) },
+        navigationIcon = { navigationIcon?.let { navigationIcon() } })
 }
 
 
